ed56523505baee554fdff6b6118d9fa751fe2fe9
Miquel44 <91468404+Miquel44@users.noreply.github.com>
2023-10-30 16:05:27 +0100

    Update Practica2.c

--- Practica2.c
+++ Practica2.c
@@ -38,3 +38,24 @@ void PrintVect(float vect[N], int from, int numel ){
 }
 
 
+void PrintRow(float mat[N][N],int row, int from, int numel ){
+	int i; // Declarem la variable i com a int.
+	for (i = from; i < numel + from; i++){ // Igualem la variable i al valor introduit from per començar a imprimir per pantalla desde aquella posició amb la condicio que i no passi del valor from + numel, la variable numel es la cantitat de valors que imprimira a partir del from.
+		printf ("%f\n", mat[row][i]); // Imprimim el valor que hi ha a mat[row][i].
+	}
+	printf("\n"); // Salt de linea per llegir millor el resultat.
+}
+
+void MultEscalar(float vect[N], float vectres[N], float alfa){
+	int i; // Declarem la variable i com a int.
+	for (i = 0; i < N; i++){ // Per cada valor del vect amb mida N.
+		vectres[i] = vect[i]*alfa; // Es multiplica el valor a vect[i] per alfa i es guarda a la mateixa posició i en un altre vector.
+	}
+}
+
+float Scalar(float vect1[N], float vect2[N]){
+	int suma, i; // Declarem les variables suma i "i" com a int.
+	for (i = 0; i < N; i++){ // Per cada valor del vect amb mida N.
+		suma += vect1[i]*vect2[i]; // Es multiplica el valor a vect1[i] per el valor a vect2[i] i es fa un sumatori de tots els valors.
+	}
+	return suma; // Retorna el resultat del sumatori
