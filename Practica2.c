b0419c24b018e404d8facdf1331732071812af6d
Miquel44 <91468404+Miquel44@users.noreply.github.com>
2023-11-10 17:06:08 +0100

    Update Practica2.c Final
    Final commit

--- Practica2.c
+++ Practica2.c
@@ -1,16 +1,16 @@
-// Importem les llibreries necessaries.
+// Importamos las bibliotecas necesarias.
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-// Definim N = 512.
+// Definimos N = 512.
 #define N 512
-// Declarem les Matrius.
+// Declaramos las Matrices.
 float Mat[N][N], MatDD[N][N];
-// Declarem els Vectors.
+// Declaramos los Vectores.
 float V1[N], V2[N], V3[N], V4[N];
 
-// Inicialització Matrius i Vectors.
+// Inicialización de Matrices y Vectores.
 void InitData(){
     int i,j;
     srand(4422543);
@@ -28,137 +28,147 @@ void InitData(){
         V2[i]=(i>=N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
         V3[i]=(((i*j)%5)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
     }
-
 }
 
-
+// Imprime un vector dado, desde 'from' hasta 'numel'.
 void PrintVect(float vect[N], int from, int numel ){
-    int i; // Declarem i com a int.
-    for (i = from; i <= numel + from; i++){ // Igualem la variable i al valor introduit from per començar a imprimir per pantalla desde aquella posició amb la condicio que i no passi del valor from + numel, la variable numel es la cantitat de valors que imprimira a partir del from.
-        printf ("%f\n", vect[i]); // Imprimim el valor que hi ha a vect[i].
+    int i; // Declaramos i como int.
+    for (i = from; i <= numel + from; i++){ // Igualamos la variable i al valor introducido en 'from' para comenzar a imprimir por pantalla desde esa posición con la condición de que i no supere el valor 'from + numel', la variable 'numel' es la cantidad de valores que imprimirá a partir de 'from'.
+        printf ("%f\n", vect[i]); // Imprimimos el valor que hay en vect[i].
     }
-    printf("\n"); // Salt de linea per llegir millor el resultat.
+    printf("\n"); // Salto de línea para leer mejor el resultado.
 }
 
+// Imprime una fila de una matriz dada, desde 'from' hasta 'numel'.
 void PrintRow(float mat[N][N],int row, int from, int numel ){
-    int i; // Declarem la variable i com a int.
-    for (i = from; i < numel + from; i++){ // Igualem la variable i al valor introduit from per començar a imprimir per pantalla desde aquella posició amb la condicio que i no passi del valor from + numel, la variable numel es la cantitat de valors que imprimira a partir del from.
-        printf ("%f\n", mat[row][i]); // Imprimim el valor que hi ha a mat[row][i].
+    int i; // Declaramos la variable i como int.
+    for (i = from; i < numel + from; i++){ // Igualamos la variable i al valor introducido en 'from' para comenzar a imprimir por pantalla desde esa posición con la condición de que i no supere el valor 'from + numel', la variable 'numel' es la cantidad de valores que imprimirá a partir de 'from'.
+        printf ("%f\n", mat[row][i]); // Imprimimos el valor que hay en mat[row][i].
     }
-    printf("\n"); // Salt de linea per llegir millor el resultat.
+    printf("\n"); // Salto de línea para leer mejor el resultado.
 }
 
+// Multiplica un vector por un escalar y almacena el resultado en otro vector.
 void MultEscalar(float vect[N], float vectres[N], float alfa){
-    int i; // Declarem la variable i com a int.
-    for (i = 0; i < N; i++){ // Per cada valor del vect amb mida N.
-        vectres[i] = vect[i]*alfa; // Es multiplica el valor a vect[i] per alfa i es guarda a la mateixa posició i en un altre vector.
+    int i; // Declaramos la variable i como int.
+    for (i = 0; i < N; i++){ // Para cada valor del vector con tamaño N.
+        vectres[i] = vect[i]*alfa; // Se multiplica el valor en vect[i] por alfa y se guarda en la misma posición y en otro vector.
     }
 }
 
+// Calcula el producto escalar entre dos vectores.
 float Scalar(float vect1[N], float vect2[N]){
-    int suma, i; // Declarem les variables suma i "i" com a int.
-    for (i = 0; i < N; i++){ // Per cada valor del vect amb mida N.
-        suma += vect1[i]*vect2[i]; // Es multiplica el valor a vect1[i] per el valor a vect2[i] i es fa un sumatori de tots els valors.
+    int suma, i; // Declaramos las variables suma e "i" como int.
+    for (i = 0; i < N; i++){ // Para cada valor del vector con tamaño N.
+        suma += vect1[i]*vect2[i]; // Se multiplica el valor en vect1[i] por el valor en vect2[i] y se realiza un sumatorio de todos los valores.
     }
-    return suma; // Retorna el resultat del sumatori.
+    return suma; // Retorna el resultado del sumatorio.
 }
 
+// Calcula la magnitud de un vector.
 float Magnitude(float vect[N] ){
-    int suma, i; // Declarem les variables suma i "i" com a int.
-    for (i = 0; i < N; i++){ // Per cada valor del vect amb mida N.
-        suma += vect[i]*vect[i]; // S'eleva al quadrat el valor a vect[i] i es fa un sumatori de tots els valors.
+    int suma, i; // Declaramos las variables suma e "i" como int.
+    for (i = 0; i < N; i++){ // Para cada valor del vector con tamaño N.
+        suma += vect[i]*vect[i]; // Se eleva al cuadrado el valor en vect[i] y se realiza un sumatorio de todos los valores.
     }
-    return suma; // Retorna el resultat del sumatori.
+    return suma; // Retorna el resultado del sumatorio.
 }
 
+// Comprueba si dos vectores son ortogonales.
 int Ortogonal(float vect1[N], float vect2[N]){
-    if (Scalar(vect1, vect2) == 0){ // Si el valor retornat per la funció Scalar() és 0.
+    if (Scalar(vect1, vect2) == 0){ // Si el valor retornado por la función Scalar() es 0.
         return 1; // Retorna 1 (True).
     }
-    else{ // Si no és el cas.
-        return 0;// Retorna 0 (False).
+    else{ // Si no es el caso.
+        return 0; // Retorna 0 (False).
     }
 }
 
+// Proyecta un vector sobre otro.
 void Projection( float vect1[N], float vect2[N], float vectres[N] ){
-    float uv, v, div; // Declarem les variables uv, v, i div com a float.
-    uv = Scalar(vect1, vect2); // Calculem el producte escalar entre els dos vectors amb la funció Scalar() i guardem el resultat a la variable uv.
-    v = Magnitude(vect2); // Calculem la magnitud del segon vector amb la funció Magnitude() i la guardem a la variable v.
-    div = uv / v; // Dividim el producte escalar dels dos vectors entre la magnitud del segon i ho guardem a la variable div.
-    MultEscalar(vect2, vectres, div); // Amb la funció MultEscalar() fem la multiplicació del segon vector per un escalar, que en aquest cas és el valor guardat a div.
+    float uv, v, div; // Declaramos las variables uv, v, y div como float.
+    uv = Scalar(vect1, vect2); // Calculamos el producto escalar entre los dos vectores con la función Scalar() y guardamos el resultado en la variable uv.
+    v = Magnitude(vect2); // Calculamos la magnitud del segundo vector con la función Magnitude() y la guardamos en la variable v.
+    div = uv / v; // Dividimos el producto escalar de los dos vectores entre la magnitud del segundo y lo guardamos en la variable div.
+    MultEscalar(vect2, vectres, div); // Con la función MultEscalar() realizamos la multiplicación del segundo vector por un escalar, que en este caso es el valor guardado en div.
 }
 
+// Calcula la norma infinito de una matriz.
 float Infininorm(float M[N][N]){
-    float max = 0.0, sum; // Declarem les variables max i sum com a float, inicialitzant també max a 0.0.
-    int i, j; // Declarem les variables "i" i j com a int.
+    float max = 0.0, sum; // Declaramos las variables max y sum como float, inicializando también max a 0.0.
+    int i, j; // Declaramos las variables "i" y j como int.
     for (i = 0; i < N; i++){
         sum = 0.0;
         for (j = 0; j < N-1; j++){
-            sum += fabs(M[i][j]); // Es suma cada valor absolut d'una fila.
+            sum += fabs(M[i][j]); // Se suma cada valor absoluto de una fila.
         }
-        if (sum > max){ // Si la suma d'una fila és més gran que el maxim (la suma d'una fila més alta fins ara).
-            max = sum; // La suma d'aquella fila és el nou máxim.
+        if (sum > max){ // Si la suma de una fila es mayor que el máximo (la suma de una fila más alta hasta ahora).
+            max = sum; // La suma de esa fila es el nuevo máximo.
         }
     }
-    return max; // Retorna el valor max.
+    return max; // Retorna el valor máximo.
 }
 
+// Calcula la norma uno de una matriz.
 float Onenorm(float M[N][N]){
-    float max = 0.0, sum; // Declarem les variables max i sum com a float, inicialitzant també max a 0.0.
-    int i, j; // Declarem les variables "i" i j com a int.
+    float max = 0.0, sum; // Declaramos las variables max y sum como float, inicializando también max a 0.0.
+    int i, j; // Declaramos las variables "i" y j como int.
     for (j = 0; j < N; j++){
         sum = 0.0;
         for (i = 0; i < N-1; i++){
-            sum += fabs(M[i][j]); // Es suma cada valor absolut d'una columna.
+            sum += fabs(M[i][j]); // Se suma cada valor absoluto de una columna.
         }
-        if (sum > max){ // Si la suma d'una columna és més gran que el maxim (la suma d'una columna més alta fins ara).
-            max = sum; // La suma d'aquella columna és el nou máxim.
+        if (sum > max){ // Si la suma de una columna es mayor que el máximo (la suma de una columna más alta hasta ahora).
+            max = sum; // La suma de esa columna es el nuevo máximo.
         }
     }
-    return max; // Retorna el valor max.
+    return max; // Retorna el valor máximo.
 }
 
+// Calcula la norma de Frobenius de una matriz.
 float NormFrobenius(float M[N][N]){
-    float sum; // Declarem la variable sum com a float.
-    int i, j; // Declarem les variables "i" i j com a int.
+    float sum; // Declaramos la variable sum como float.
+    int i, j; // Declaramos las variables "i" y j como int.
     for (i = 0; i < N; i++){
         for (j = 0; j < N-1; j++){
-            sum += M[i][j] * M[i][j]; // Es fa un sumatori de cada valor de la matriu elavat al quadrat.
+            sum += M[i][j] * M[i][j]; // Se realiza un sumatorio de cada valor de la matriz elevado al cuadrado.
         }
     }
-    float arrel = sqrt(sum); // Declarem la variable arrel com a float i la igualem a l'arrel quadrada del sumatori anterior.
-    return arrel; // Retornem el valor que hi ha a arrel.
+    float raiz = sqrt(sum); // Declaramos la variable raiz como float y la igualamos a la raíz cuadrada del sumatorio anterior.
+    return raiz; // Retornamos el valor que hay en raiz.
 }
 
+// Comprueba si una matriz es diagonal dominante.
 int DiagonalDom(float M[N][N]){
-    float ediag, sum_no_diag; // Declarem les variables ediag(Element de la diagonal) i sum_no_diag(suma elements que no son de la diagonal).
-    int i, j; // Declarem les variables "i" i j com a int.
+    float ediag, sum_no_diag; // Declaramos las variables ediag (Elemento de la diagonal) y sum_no_diag (suma elementos que no son de la diagonal).
+    int i, j; // Declaramos las variables "i" y j como int.
     for (i = 0; i < N; i++){
-    	sum_no_diag = 0.0;
+        sum_no_diag = 0.0;
         for (j = 0; j < N; j++){
             if (i == j){
-                ediag = fabs(M[i][j]); // S'iguala a ediag el valor del element de la diagonal de la fila i.
+                ediag = fabs(M[i][j]); // Se iguala a ediag el valor del elemento de la diagonal de la fila i.
             }
             else{
-                sum_no_diag += fabs(M[i][j]); // Es sumen tots els valors que no son de la diagonal a la variable sum_no_diag.
+                sum_no_diag += fabs(M[i][j]); // Se suman todos los valores que no son de la diagonal a la variable sum_no_diag.
             }
         }
-        if (ediag < sum_no_diag){ // Si el valor de la diagonal és més petit que la suma de tots els altres valors de la fila.
-            return 0; // Retorna 0 (False)
-        } // Només cal que una fila compleixi la condició per fer que la matriu no sigui diagonal dominant.
-
+        if (ediag < sum_no_diag){ // Si el valor de la diagonal es menor que la suma de todos los otros valores de la fila.
+            return 0; // Retorna 0 (False).
+        } // Solo es necesario que una fila cumpla la condición para hacer que la matriz no sea diagonal dominante.
     }
-    return 1; // Retorna 1 (True)
+    return 1; // Retorna 1 (True).
 }
+
+// Implementa el método de Jacobi para resolver un sistema lineal.
 int Jacobi( float M[N][N] , float vect[N], float vectres[N], unsigned iter ) {
   // Declarar variables locales
   float x[N]; // Vector de soluciones aproximadas
   float error; // Error entre dos soluciones consecutivas
   float sum; // Suma parcial de los productos de los elementos de la matriz y el vector
-  int i, j, k; // Indices para recorrer la matriz y el vector
+  int i, j, k; // Índices para recorrer la matriz y el vector
   int converge = 1; // Indicador de convergencia
   if (DiagonalDom(M) == 0) {
-      printf("La matriu Mat no és diagonal dominant. No es pot aplicar Jacobi.\n");
+      printf("La matriz Mat no es diagonal dominante. No se puede aplicar Jacobi.\n");
       return 0;
       exit(0);
   }
@@ -166,10 +176,7 @@ int Jacobi( float M[N][N] , float vect[N], float vectres[N], unsigned iter ) {
   for (i = 0; i < N; i++) {
     x[i] = 0;
   }
-
-
   for (k = 0; k < iter; k++) {
-
     for (i = 0; i < N; i++) {
       sum = 0;
       for (j = 0; j < N; j++) {
@@ -179,33 +186,26 @@ int Jacobi( float M[N][N] , float vect[N], float vectres[N], unsigned iter ) {
       }
       vectres[i] = (vect[i] - sum) / M[i][i];
     }
-
-
+    // Calcular el error entre dos soluciones consecutivas, PUNTO EXTRA
     error = 0;
     for (i = 0; i < N; i++) {
       error += fabs(vectres[i] - x[i]);
     }
-
-
+    // Comprobar si el error es suficientemente pequeño para convergencia, PUNTOEXTRA
     if (error < 0.0001) {
       break;
     }
-
-
+    // Actualizar el vector de soluciones con el nuevo resultado
     for (i = 0; i < N; i++) {
       x[i] = vectres[i];
     }
   }
-
-
+  // Si el número de iteraciones alcanza el límite, considerar que no converge
   if (k == iter) {
     converge = 0;
   }
-
-
   return converge;
 }
-
 int main(){
     InitData();
 
