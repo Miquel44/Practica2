db78646168eccd12c29abd9e1bdb1ec0cca7c5b3
Miquel44 <91468404+Miquel44@users.noreply.github.com>
2023-12-14 17:36:49 +0100

    Update my_mv.c

--- my_mv.c
+++ my_mv.c
@@ -9,12 +9,12 @@ void main(){
     char nom_fitxer[20], nom_fitxer2[20],fitxer[100000],dir[20]; // Declarem les variables on anira el nom del fitxer i el contingut del fitxer respectivament.
     printf("Escriu el nom del fitxer:\n");
     scanf("%s", nom_fitxer); // Llegim el nom del fitxer donat per teclat.
-    printf("Escriu el nom del fitxer:\n");
+    printf("Escriu el nou nom del fitxer:\n");
     scanf("%s", nom_fitxer2); // Llegim el nom del fitxer donat per teclat.
-    printf("Escriu la nova direccio");
+    printf("Escriu la nova direccio:\n");
     scanf("%s", dir);
     int fd = open(nom_fitxer, O_RDONLY); // Obrim el fitxer.
-    int desc = open(dir, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+    int desc = open(strcat(dir,nom_fitxer2), O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
 
     if (fd==-1){ // Si fd = -1 vol dir que hi ha agut un error i per tant que el fitxer no existeix.
         printf("Fitxer no trobat\n");
@@ -23,11 +23,11 @@ void main(){
         ssize_t n = read(fd,fitxer,100000); // Llegim el fitxer i emmagatzem el contingut a fitxer
 
         write(desc, fitxer, n);
-        close(fd)
+        close(fd);
         unlink(nom_fitxer); 
     }
-    }
-    if (desc != -1) {
+    
+    if (desc != -1){
         close(desc); // Tanquem la direccio
     }
 
